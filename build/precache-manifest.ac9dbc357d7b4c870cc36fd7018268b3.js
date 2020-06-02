2c94e553c44366ea1e77aa64eaff71c26d1db16a
danikone63 <62764663+danikone63@users.noreply.github.com>
2020-06-02 13:58:08 +0300

    Add files via upload

---
+++ build/precache-manifest.ac9dbc357d7b4c870cc36fd7018268b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6705a54c0bd4c1017686bb090146bdb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b20a131b4657b2adb33c",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "67c08b5fcbd0138eaeff",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b20a131b4657b2adb33c",
+    "url": "./static/js/2.aff53663.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.aff53663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67c08b5fcbd0138eaeff",
+    "url": "./static/js/main.ccd991d1.chunk.js"
+  },
+  {
+    "revision": "58103c197818ae26b331",
+    "url": "./static/js/runtime-main.e7c7b99b.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
